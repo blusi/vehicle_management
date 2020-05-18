93fbd5fe4d1a99237e1155832c16304e868855c0
jerry_onelive <5481582+jerry_onelive@user.noreply.gitee.com>
2020-05-18 10:44:23 +0800

    第一次提交

---
+++ src/main/java/com/xinhong/buildcontrol/pojo/Car.java
@@ -0,0 +1,163 @@
+package com.xinhong.buildcontrol.pojo;
+
+import lombok.Data;
+
+/**
+ * 汽车实体
+ */
+@Data
+public class Car {
+    private String carId;
+    private String carNumber;
+    private String carBrand;
+    private String carName;
+    private String carType;
+    private String carStatus;
+    private int carAge;
+    private String carDepartment;
+    private String carPossessor;
+    private String carEngineNumber;
+    private String carFunction;
+    private String carVin;
+    private String carRegistrationDate;
+    private String carLicenceIssued;
+    private String carOperator;
+    private String carModified;
+    private String carCreate;
+
+    public String getCarId() {
+        return carId;
+    }
+
+    public void setCarId(String carId) {
+        this.carId = carId;
+    }
+
+    public String getCarNumber() {
+        return carNumber;
+    }
+
+    public void setCarNumber(String carNumber) {
+        this.carNumber = carNumber;
+    }
+
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
+
+    public String getCarStatus() {
+        return carStatus;
+    }
+
+    public void setCarStatus(String carStatus) {
+        this.carStatus = carStatus;
+    }
+
+    public int getCarAge() {
+        return carAge;
+    }
+
+    public void setCarAge(int carAge) {
+        this.carAge = carAge;
+    }
+
+    public String getCarDepartment() {
+        return carDepartment;
+    }
+
+    public void setCarDepartment(String carDepartment) {
+        this.carDepartment = carDepartment;
+    }
+
+    public String getCarPossessor() {
+        return carPossessor;
+    }
+
+    public void setCarPossessor(String carPossessor) {
+        this.carPossessor = carPossessor;
+    }
+
+    public String getCarEngineNumber() {
+        return carEngineNumber;
+    }
+
+    public void setCarEngineNumber(String carEngineNumber) {
+        this.carEngineNumber = carEngineNumber;
+    }
+
+    public String getCarFunction() {
+        return carFunction;
+    }
+
+    public void setCarFunction(String carFunction) {
+        this.carFunction = carFunction;
+    }
+
+    public String getCarVin() {
+        return carVin;
+    }
+
+    public void setCarVin(String carVin) {
+        this.carVin = carVin;
+    }
+
+    public String getCarRegistrationDate() {
+        return carRegistrationDate;
+    }
+
+    public void setCarRegistrationDate(String carRegistrationDate) {
+        this.carRegistrationDate = carRegistrationDate;
+    }
+
+    public String getCarLicenceIssued() {
+        return carLicenceIssued;
+    }
+
+    public void setCarLicenceIssued(String carLicenceIssued) {
+        this.carLicenceIssued = carLicenceIssued;
+    }
+
+    public String getCarOperator() {
+        return carOperator;
+    }
+
+    public void setCarOperator(String carOperator) {
+        this.carOperator = carOperator;
+    }
+
+    public String getCarModified() {
+        return carModified;
+    }
+
+    public void setCarModified(String carModified) {
+        this.carModified = carModified;
+    }
+
+    public String getCarCreate() {
+        return carCreate;
+    }
+
+    public void setCarCreate(String carCreate) {
+        this.carCreate = carCreate;
+    }
+}
