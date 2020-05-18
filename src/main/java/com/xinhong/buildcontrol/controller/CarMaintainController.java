50cc98c2477354bc47df6fb5fddfa31b36bb4c0b
jerry_onelive <5481582+jerry_onelive@user.noreply.gitee.com>
2020-05-18 15:36:01 +0800

    第三次提交

--- src/main/java/com/xinhong/buildcontrol/controller/CarMaintainController.java
+++ src/main/java/com/xinhong/buildcontrol/controller/CarMaintainController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @Api(tags = "保养管理")
 @RestController
-@RequestMapping("car/carInsurance")
+@RequestMapping("car/carMaintain")
 @CrossOrigin
 public class CarMaintainController {
 
